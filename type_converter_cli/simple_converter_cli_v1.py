554e1765890c66b126e1ad67466161fd6a1676dd
Biniam Seifu <biniamurrogrammer@gmail.com>
2025-12-17 21:34:41 +0300

    Add files via upload

---
+++ type_converter_cli/simple_converter_cli_v1.py
@@ -0,0 +1,10 @@
+
+
+number = input("Enter a Number to Convert: ")
+
+print("Integer: ", int(number))
+print("Float: ", float(number))
+print("String: ", str(number))
+print("Boolean: ", bool(number))
+print("List: ", list(number))
+
