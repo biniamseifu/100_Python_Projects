9a07ae284b13c8c7f2391355cacec70747c99760
Biniam Seifu <biniamurrogrammer@gmail.com>
2025-12-19 20:57:55 +0300

    Add files via upload

---
+++ login_system/login_system_v1.py
@@ -0,0 +1,9 @@
+
+user_name = input("Enter Your UserName: ")
+pwd = input("Enter Your Password: ")
+
+if user_name == "admin" and pwd == "admin1234":
+    print("Log-in Completed Successfully")
+else:
+    print("Wrong Password or Username")
+
