d631b6a68acb92473e8e4cadb106c93fdd830fc1
Biniam Seifu <biniamurrogrammer@gmail.com>
2025-12-19 20:57:30 +0300

    Add files via upload

---
+++ even_or_odd/even_or_odd_v1.py
@@ -0,0 +1,8 @@
+
+num = int(input("Enter a Numebr: "))
+
+if num % 2 == 0:
+    print(f"{num} is Even")
+else:
+    print(f"{num} is Odd")
+
---
+++ grade_checker/grade_checker_v1.py
@@ -0,0 +1,14 @@
+
+score = int(input("Enter Your Score: "))
+
+if score >= 90:
+    print("Your Grade is: A")
+elif score >= 80:
+    print("Your Grade is: B")
+elif score >= 70:
+    print("Your Grade is: C")
+elif score >= 60:
+    print("Your Grade is: D")
+else:
+    print("F : Failed")
+
